3a66eb8768023e2bdfaf0eb6d1d373f16ea907e2
Lushito <luisdevcol@gmail.com>
2024-06-28 14:35:36 -0500

    feat: Add Header component with dynamic button based on user sign-in status

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+import { UserButton, useUser } from "@clerk/clerk-react";
 
 const Header = () => {
+  const { user, isSignedIn } = useUser();
   return (
     <div className="p-3 px-5 flex justify-between shadow-md">
       <img src="/logo.svg" height={60} width={60}></img>
-      <Button>Get Started</Button>
+      {isSignedIn ? (
+        <div className="flex gap-2 items-center">
+          <Link to="/dashboard">
+            <Button variant="outline">Dashboard</Button>
+          </Link>
+          <UserButton />
+        </div>
+      ) : (
+        <Link to="/auth/sign-in">
+          <Button>Get Started</Button>
+        </Link>
+      )}
     </div>
   );
 };
